75f523096a571b7af9384873474031c16d68ef49
sumin <smin.geem@gmail.com>
2024-06-13 20:43:36 +0900

    #2. 온보딩 swiper 추가

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, session
 import sqlite3
 import requests
+from config import CLIENT_ID, SECRET_KEY, REDIRECT_URI
 
 # 로그인
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
+
 def get_db():
     conn = sqlite3.connect(app.config['DATABASE_PATH'])
     return conn
+
 
 # 카카오 설정
 KAKAO_OAUTH_URL = 'https://kauth.kakao.com/oauth/authorize'
 KAKAO_TOKEN_URL = 'https://kauth.kakao.com/oauth/token'
 KAKAO_PROFILE_URL = 'https://kapi.kakao.com/v2/user/me'
-CLIENT_ID = '896ccf1efb303a2172d555d7f7036c71'
-REDIRECT_URI = 'http://localhost:5001/oauth'
+
 
 def init_db():
     with app.app_context():
@@ -25,7 +27,6 @@ def init_db():
             CREATE TABLE IF NOT EXISTS users (
                 id INTEGER PRIMARY KEY,
                 kakao_id TEXT,
-                email TEXT,
                 nickname TEXT
             )
         ''')
@@ -56,35 +57,38 @@ def oauth():
     }
     token_response = requests.post(KAKAO_TOKEN_URL, data=token_data)
     token_json = token_response.json()
-    
+
     if token_response.status_code != 200:
         return f"Error getting token: {token_response.status_code}, {token_json}"
-    
+
     access_token = token_json['access_token']
     profile_headers = {
         'Authorization': f'Bearer {access_token}',
     }
-    
+
     profile_response = requests.get(KAKAO_PROFILE_URL, headers=profile_headers)
     profile_json = profile_response.json()
-    
+
     if profile_response.status_code != 200:
         return f"Error getting profile: {profile_response.status_code}, {profile_json}"
-    
+
     kakao_id = profile_json['id']
-    email = profile_json.get('kakao_account', {}).get('email')
     nickname = profile_json.get('properties', {}).get('nickname')
-    
+
     db = get_db()
     cursor = db.cursor()
     cursor.execute('SELECT * FROM users WHERE kakao_id = ?', (kakao_id,))
     user = cursor.fetchone()
     if not user:
-        cursor.execute('INSERT INTO users (kakao_id, email, nickname) VALUES (?, ?, ?)', (kakao_id, email, nickname))
+        cursor.execute(
+            'INSERT INTO users (kakao_id, email, nickname) VALUES (?, ?, ?)', (kakao_id, nickname))
         db.commit()
-    
+
     session['kakao_id'] = kakao_id
     session['nickname'] = nickname
+
+    session.permanent = True
+    app.permanent_session_lifetime = app.config['PERMANENT_SESSION_LIFETIME']
     return redirect(url_for('main'))
 
 
@@ -100,12 +104,16 @@ def logout():
     session.clear()
     return redirect(url_for('home'))
 
+
 @app.route('/main')
 def main():
     if 'kakao_id' not in session or 'nickname' not in session:
         return redirect(url_for('home'))
     return render_template('main.html', nickname=session['nickname'])
 
+@app.route('/test')
+def test():
+    return render_template('test.html')
 
 if __name__ == '__main__':
     init_db()
--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 from datetime import timedelta
 
 SECRET_KEY = '108b685669703a108f453dafdce3caa'
+CLIENT_ID = '896ccf1efb303a2172d555d7f7036c71'
+REDIRECT_URI = 'http://localhost:5001/oauth'
+
 DATABASE_PATH = 'database.db'
-PERMANENT_SESSION_LIFETIME = timedelta(minutes=10)
+PERMANENT_SESSION_LIFETIME = timedelta(minutes=1)
